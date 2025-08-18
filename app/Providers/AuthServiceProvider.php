b4b5e241fc1ebd2db0c2fb9cfc7012511c87cd7f
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 23:16:03 +0700

    feat(auth): add policies for Report and Village models in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,20 @@ namespace App\Providers;
 
 use App\Models\District;
 use App\Models\Opd;
+use App\Models\Report;
+use App\Models\ReportAttachment;
+use App\Models\ReportCategory;
+use App\Models\ReportStatusHistory;
+use App\Models\ReportType;
+use App\Models\Village;
 use App\Policies\DistrictPolicy;
 use App\Policies\OpdPolicy;
+use App\Policies\ReportAttachmentPolicy;
+use App\Policies\ReportCategoryPolicy;
+use App\Policies\ReportPolicy;
+use App\Policies\ReportStatusHistoryPolicy;
+use App\Policies\ReportTypePolicy;
+use App\Policies\VillagePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -14,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Opd::class => OpdPolicy::class,
         District::class => DistrictPolicy::class,
+        ReportAttachment::class => ReportAttachmentPolicy::class,
+        ReportCategory::class => ReportCategoryPolicy::class,
+        Report::class => ReportPolicy::class,
+        ReportStatusHistory::class => ReportStatusHistoryPolicy::class,
+        ReportType::class => ReportTypePolicy::class,
+        Village::class => VillagePolicy::class,
     ];
 
     /**
