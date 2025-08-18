539b6d570ae13aec2c94db94b11c96a40d134dde
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 08:17:39 +0700

    Add (UserRole): enum to define user roles in the application

---
+++ app/Enums/UserRole.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRole: string
+{
+    case CITIZEN = 'CITIZEN';
+    case FIELD_OFFICER = 'FIELD_OFFICER';
+    case QC_OFFICER = 'QC_OFFICER';
+    case OPD_ADMIN = 'OPD_ADMIN';
+    case DISTRICT_ADMIN = 'DISTRICT_ADMIN';
+    case CITY_ADMIN = 'CITY_ADMIN';
+}
