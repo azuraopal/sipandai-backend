e4d3dd6eaf132e1369d2d884e44a220ceac81d86
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 13:56:12 +0700

    feat(routes): add Google authentication routes for redirect and callback

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GoogleAuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('auth.google');
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
