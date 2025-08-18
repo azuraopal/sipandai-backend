5396d4661084e08a20842d3e8cca27a8cb0c1060
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 15:50:40 +0700

    feat(report): add ReportStatusHistoryController for managing report status history

---
+++ app/Http/Controllers/Api/ReportStatusHistoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\ReportStatusHistory;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReportStatusHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReportStatusHistory $reportStatusHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ReportStatusHistory $reportStatusHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReportStatusHistory $reportStatusHistory)
+    {
+        //
+    }
+}
