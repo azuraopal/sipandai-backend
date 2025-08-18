b79db83674836d598077de0e60dfb71faab137a2
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 13:55:32 +0700

    feat(migrations): change avatar field type from string to text in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('password')->nullable();
             $table->string('role')->default(UserRole::CITIZEN->value);
             $table->string('profile_picture_url')->nullable();
-            $table->string('avatar')->nullable();
+            $table->text('avatar')->nullable();
             $table->string('district_id', 2)->nullable();
             $table->rememberToken();
             $table->timestamps();
