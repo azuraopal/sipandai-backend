37b651e1ad0fe8f9bf4241dcbc8e3dd58eb45b36
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 23:55:32 +0700

    perf(Seed): Change format District code

--- database/seeders/DistrictSeeder.php
+++ database/seeders/DistrictSeeder.php
@@ -2,32 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class DistrictSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run()
+    public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
         DB::table('districts')->truncate();
 
         $districts = [
-            ['code' => '01', 'name' => 'Kecamatan Bandung Wetan'],
-            ['code' => '02', 'name' => 'Kecamatan Cibeunying Kidul'],
-            ['code' => '03', 'name' => 'Kecamatan Sukasari'],
-            ['code' => '04', 'name' => 'Kecamatan Gedebage'],
-            ['code' => '05', 'name' => 'Kecamatan Andir'],
-            ['code' => '06', 'name' => 'Kecamatan Babakan Ciparay'],
-            ['code' => '07', 'name' => 'Kecamatan Cicadas'],
-            ['code' => '08', 'name' => 'Kecamatan Batununggal'],
-            ['code' => '09', 'name' => 'Kecamatan Kopo'],
-            ['code' => '10', 'name' => 'Kecamatan Lengkong'],
+            ['code' => '01', 'name' => 'Kecamatan Bandung Wetan', 'created_at' => now()],
+            ['code' => '02', 'name' => 'Kecamatan Cibeunying Kidul', 'created_at' => now()],
+            ['code' => '03', 'name' => 'Kecamatan Sukasari', 'created_at' => now()],
+            ['code' => '04', 'name' => 'Kecamatan Gedebage', 'created_at' => now()],
+            ['code' => '05', 'name' => 'Kecamatan Andir', 'created_at' => now()],
+            ['code' => '06', 'name' => 'Kecamatan Babakan Ciparay', 'created_at' => now()],
+            ['code' => '07', 'name' => 'Kecamatan Cicadas', 'created_at' => now()],
+            ['code' => '08', 'name' => 'Kecamatan Batununggal', 'created_at' => now()],
+            ['code' => '09', 'name' => 'Kecamatan Kopo', 'created_at' => now()],
+            ['code' => '10', 'name' => 'Kecamatan Lengkong', 'created_at' => now()],
         ];
 
         DB::table('districts')->insert($districts);
