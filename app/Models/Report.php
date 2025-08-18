d817c93df9199d32ad8bb3f61e6d5bdf769793f3
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 15:50:22 +0700

    feat(report): implement Report model, controller, and migration with relationships and status handling

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,9 +2,67 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Report extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'report_code',
+        'user_id',
+        'type_id',
+        'category_id',
+        'title',
+        'description',
+        'district_id',
+        'village_id',
+        'address_detail',
+        'phone_number',
+        'coordinates',
+        'current_status',
+    ];
+
+    protected $casts = [
+        'current_status' => ReportStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reportType(): BelongsTo
+    {
+        return $this->belongsTo(ReportType::class, 'type_id');
+    }
+
+    public function reportCategory(): BelongsTo
+    {
+        return $this->belongsTo(ReportCategory::class, 'category_id');
+    }
+
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(District::class, 'district_id', 'code');
+    }
+
+    public function village(): BelongsTo
+    {
+        return $this->belongsTo(Village::class, 'village_id', 'code');
+    }
+
+    public function statusHistories(): HasMany
+    {
+        return $this->hasMany(ReportStatusHistory::class);
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasMany(ReportAttachment::class);
+    }
 }
--- database/migrations/2025_08_18_080113_create_reports_table.php
+++ database/migrations/2025_08_18_080113_create_reports_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\ReportStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reports', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('report_code', 8)->unique();
+            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignId('type_id')->constrained('report_types');
+            $table->foreignId('category_id')->constrained('report_categories');
+            $table->string('district_id', 2);
+            $table->foreign('district_id')->references('code')->on('districts');
+            $table->string('village_id', 5);
+            $table->foreign('village_id')->references('code')->on('villages');
+            $table->string('title');
+            $table->text('description');
+            $table->text('address_detail');
+            $table->string('phone_number', 15);
+            $table->point('coordinates');
+            $table->string('current_status')->default(ReportStatus::PENDING_VERIFICATION->value);
             $table->timestamps();
         });
     }
