2043586edd8ac47e5f61256e506b8295fa09fd97
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 15:51:09 +0700

    feat(village): add VillageController and migration for villages table

--- database/migrations/2025_08_18_032835_create_villages_table.php
+++ database/migrations/2025_08_18_032835_create_villages_table.php
@@ -18,7 +18,6 @@ return new class extends Migration {
                 ->on('districts')
                 ->onUpdate('cascade')
                 ->onDelete('restrict');
-
             $table->string('name', 255);
             $table->timestamps();
         });
