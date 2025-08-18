d8035d4ea34dd91da2a89dd4a58bf4251d19e13c
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 15:49:23 +0700

    feat(report): add ReportAttachment model, controller, and migration for handling report attachments

--- app/Models/ReportAttachment.php
+++ app/Models/ReportAttachment.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReportAttachment extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    public $timestamps = false;
+    const CREATED_AT = 'created_at';
+
+    protected $fillable = [
+        'report_id',
+        'purpose',
+        'file_url',
+        'file_type',
+    ];
+
+    protected $casts = [
+        'purpose' => AttachmentPurpose::class,
+    ];
+
+    public function report(): BelongsTo
+    {
+        return $this->belongsTo(Report::class);
+    }
 }
--- database/migrations/2025_08_18_080252_create_report_attachments_table.php
+++ database/migrations/2025_08_18_080252_create_report_attachments_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('report_attachments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('report_id')->constrained('reports')->onDelete('cascade');
+            $table->string('purpose');
+            $table->string('file_url');
+            $table->string('file_type', 50);
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
