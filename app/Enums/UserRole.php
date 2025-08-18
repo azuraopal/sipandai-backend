c15c6a656a315733d026eb787bd4533d486cc174
azuraopal <muhammadnovals334@gmail.com>
2025-08-18 10:06:13 +0700

    refactor(UserRole and migration): update DISTRICT_ADMIN case and set created_at to use current timestamp

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -8,6 +8,6 @@ enum UserRole: string
     case FIELD_OFFICER = 'FIELD_OFFICER';
     case QC_OFFICER = 'QC_OFFICER';
     case OPD_ADMIN = 'OPD_ADMIN';
-    case DISTRICT_ADMIN = 'DISTRICT_ADMIN';
+    case DISTRICT_ADMIN = 'DISTRICT_ADMIN'; 
     case CITY_ADMIN = 'CITY_ADMIN';
 }
--- database/migrations/2025_08_18_023136_create_districts_table.php
+++ database/migrations/2025_08_18_023136_create_districts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('districts', function (Blueprint $table) {
             $table->string('code', 2)->primary();
             $table->string('name', 255)->nullable(false);
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
