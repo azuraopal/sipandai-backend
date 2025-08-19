916ec7ed7d43dcfd4a1f742c2eb06de94eed3a4b
azuraopal <muhammadnovals334@gmail.com>
2025-08-19 21:40:51 +0700

    refactor(routes): remove Google authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\GoogleAuthController;
-
-Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('auth.google');
-Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
