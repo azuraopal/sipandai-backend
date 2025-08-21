7c6b8a4f0e648a84d2eacd5e005a9291d1992f28
azuraopal <muhammadnovals334@gmail.com>
2025-08-21 23:11:14 +0700

    feat(ReportAssignmentController): add CRUD operations skeleton for report assignments

---
+++ app/Http/Controllers/Api/ReportAssignmentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\ReportAssignment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReportAssignmentController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(ReportAssignment $reportAssignment)
+    {
+        //
+    }
+
+
+    public function update(Request $request, ReportAssignment $reportAssignment)
+    {
+        //
+    }
+
+    public function destroy(ReportAssignment $reportAssignment)
+    {
+        //
+    }
+}
