f2ac4690b5b8a70d329e5e21bf75c1d83ead7c29
azuraopal <muhammadnovals334@gmail.com>
2025-08-25 00:20:56 +0700

    feat(User): Add column phone_number in Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
         'profile_picture_url',
+        'phone_number',
         'district_id',
         'opd_id',
         'google_id',
