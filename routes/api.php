39b2bbf3ae367b72f58fc3ecb01903e4d9f8ad9a
azuraopal <muhammadnovals334@gmail.com>
2025-09-12 18:43:02 +0700

    perf(Routes): change Routes Assignment Group

--- routes/api.php
+++ routes/api.php
@@ -67,6 +67,8 @@ Route::prefix('v1')->group(function () {
             Route::get('{id}/attachments', [ReportController::class, 'attachments']);
             Route::get('{id}/status-history', [ReportController::class, 'statusHistories']);
 
+            Route::post('/submit', [ReportUserAssignmentController::class, 'handle']);
+
             Route::prefix('/types')->group(function () {
                 Route::post('/', [ReportTypeController::class, 'store']);
                 Route::put('/{id}', [ReportTypeController::class, 'update']);
@@ -76,7 +78,6 @@ Route::prefix('v1')->group(function () {
                 Route::put('/{id}/categories/{category}', [ReportCategoryController::class, 'update']);
                 Route::delete('/{id}/categories/{category}', [ReportCategoryController::class, 'destroy']);
 
-                Route::post('/submit', [ReportUserAssignmentController::class, 'handle']);
             });
         });
     });
